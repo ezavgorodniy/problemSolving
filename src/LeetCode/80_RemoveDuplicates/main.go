f23bcc911bb7d1083f84ec3885a613d22f12b161
Yevhen Zavhorodnii <eugurzav@mail.ru>
2021-10-26 11:35:48 +0100

    improve LeetCode #80

--- src/LeetCode/80_RemoveDuplicates/main.go
+++ src/LeetCode/80_RemoveDuplicates/main.go
@@ -5,7 +5,7 @@ Given an integer array nums sorted in non-decreasing order, remove some duplicat
 
 Since it is impossible to change the length of the array in some languages, you must instead have the result be placed in the first part of the array nums. More formally, if there are k elements after removing the duplicates, then the first k elements of nums should hold the final result. It does not matter what you leave beyond the first k elements.
 
-Return k after placing the final result in thae first k slots of nums.
+Return k after placing the final result in the first k slots of nums.
 
 Do not allocate extra space for another array. You must do this by modifying the input array in-place with O(1) extra memory.
 
@@ -31,19 +31,28 @@ import (
 )
 
 func removeDuplicates(nums []int) int {
-	if len(nums) <= 2 {
-		return len(nums)
-	}
+	curIndex := 0
+	skippedCnt := 0
+	for curIndex < len(nums) {
+		if curIndex == len(nums)-1 {
+			nums[curIndex-skippedCnt] = nums[curIndex]
+			curIndex++
+			break
+		}
 
-	k := 0
-
-	for i := 2; i < len(nums); i++ {
-		if nums[k] != nums[i] {
-			nums[k+2] = nums[i]
-			k++
+		nums[curIndex-skippedCnt] = nums[curIndex]
+		if nums[curIndex] == nums[curIndex+1] {
+			nums[curIndex-skippedCnt+1] = nums[curIndex]
+			curIndex += 2
+			for curIndex < len(nums) && nums[curIndex-1] == nums[curIndex] {
+				curIndex++
+				skippedCnt++
+			}
+		} else {
+			curIndex++
 		}
 	}
-	return k + 2
+	return len(nums) - skippedCnt
 }
 
 func main() {
